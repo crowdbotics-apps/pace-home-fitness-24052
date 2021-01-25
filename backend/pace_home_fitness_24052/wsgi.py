d82e087e2a555801439db0958f98d5091b3ff8db
Crowdbotics <team@crowdbotics.com>
2021-01-25 14:47:03 +0000

    Update models

--- backend/pace_home_fitness_24052/wsgi.py
+++ backend/pace_home_fitness_24052/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pace_home_fitness_24052.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pace_home_fitness_24052.settings")
 
 application = get_wsgi_application()
