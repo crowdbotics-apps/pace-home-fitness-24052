d8ec1f17f79f2d128b343734dbefa656ea0f2394
Crowdbotics <team@crowdbotics.com>
2021-01-29 01:55:47 +0000

    remove package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'UserProfile196446', human_name: 'User Profile', access_route: 'UserProfile196446'},
 { name: 'Maps196427', human_name: 'Maps', access_route: 'Maps196427', icon: 'map'},
 { name: 'Settings196390', human_name: 'Settings', access_route: 'Settings196390'},
-{ name: 'NotificationList196389', human_name: 'Notification List', access_route: 'NotificationList196389'},
 { name: 'Maps196388', human_name: 'Maps', access_route: 'Maps196388', icon: 'map'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import UserProfile196446Navigator from '../features/UserProfile196446/navigator';
 import Maps196427Navigator from '../features/Maps196427/navigator';
 import Settings196390Navigator from '../features/Settings196390/navigator';
-import NotificationList196389Navigator from '../features/NotificationList196389/navigator';
 import Maps196388Navigator from '../features/Maps196388/navigator';
 
 /**
@@ -21,7 +20,6 @@ const AppNavigator = {
 UserProfile196446: { screen: UserProfile196446Navigator },
 Maps196427: { screen: Maps196427Navigator },
 Settings196390: { screen: Settings196390Navigator },
-NotificationList196389: { screen: NotificationList196389Navigator },
 Maps196388: { screen: Maps196388Navigator },
 
     /** new navigators can be added here */
